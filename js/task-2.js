4f13daf749c6725aed54e3a563f1600d73198b44
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-06-07 13:37:05 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,17 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+/*----------------------------------*/
+
+const gallery = document.querySelector(".gallery");
+const galleryItems = images
+  .map(
+    ({ url, alt }) =>
+      `<li class="gallery-item">
+          <img src="${url}" alt="${alt}" class="gallery-image">
+      </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryItems);
