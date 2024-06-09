1661c859e91708ce707e3a3325487d8e4dfc88c9
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-06-09 14:17:26 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+/* ---------------------------------- */
+
+const controls = document.getElementById("controls");
+const numberInput = controls.querySelector('input[type="number"]');
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+const boxesDiv = document.getElementById("boxes");
+
+createButton.addEventListener("click", () => {
+  const qty = parseInt(numberInput.value);
+  if (qty >= 1 && qty <= 100) {
+    createBoxes(qty);
+    numberInput.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
+
+function createBoxes(qty) {
+  destroyBoxes();
+  const elements = [];
+  let size = 30;
+  for ( size ; size < (30 + qty * 10); size += 10) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    elements.push(box); 
+  }
+  if (size > 426) {
+    boxesDiv.style.width = size + 64 +"px";
+  }
+  boxesDiv.append(...elements);
+}
+
